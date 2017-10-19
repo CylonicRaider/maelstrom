77961ea90aebf3dc1660243d155c76fa709f7353
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-10-19 23:45:36 +0200

    Implement extensible request routing

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
+
 const http = require('http');
-const url = require('url');
+const router = require('./lib/router.js');
 
 function sendText(resp, text) {
   resp.setHeader('Content-Type', 'text/plain; charset=utf-8');
   resp.end(text);
 }
 
-http.createServer((req, resp) => {
-  const method = req.method;
-  const { pathname } = url.parse(req.url);
-  if (method != 'GET') {
-    resp.statusCode = 405;
-    sendText(resp, '405 Method Not Allowed');
-  } else if (pathname == '/') {
-    resp.statusCode = 200;
-    sendText(resp, 'Hello World!');
-  } else {
-    resp.statusCode = 404;
-    sendText(resp, '404 Not Found');
-  }
-}).listen(8080);
+const r = new router.Router();
+
+r.get('/', (req, resp) => {
+  resp.statusCode = 200;
+  sendText(resp, 'Hello World!');
+});
+
+// Catch-all routes
+r.route('GET', null, (req, resp) => {
+  resp.statusCode = 404;
+  sendText(resp, '404 Not Found');
+});
+r.route(null, null, (req, resp) => {
+  resp.statusCode = 405;
+  sendText(resp, '405 Method Not Allowed');
+});
+
+http.createServer(r.makeCallback()).listen(8080);
