9483cdcaefbeecfd8572a69f90993aaf338e1795
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-10-27 15:40:48 +0200

    Commence FileCache

--- main.js
+++ main.js
@@ -1,22 +1,15 @@
 
 const http = require('http');
 
-const router = require('./lib/router.js');
+const { Router } = require('./lib/router.js');
+const { stringResponder } = require('./lib/static.js');
 
-function textSender(code, text) {
-  return (req, resp) => {
-    resp.statusCode = code;
-    resp.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    resp.end(text);
-  };
-}
+const r = new Router();
 
-const r = new router.Router();
-
-r.get('/', textSender(200, 'Hello World!'));
+r.get('/', stringResponder(200, 'Hello World!'));
 
 // Catch-all routes
-r.route('GET', null, textSender(404, '404 Not Found'));
-r.route(null, null, textSender(405, '405 Method Not Allowed'));
+r.route('GET', null, stringResponder(404, '404 Not Found'));
+r.route(null, null, stringResponder(405, '405 Method Not Allowed'));
 
 http.createServer(r.makeCallback()).listen(8080);
