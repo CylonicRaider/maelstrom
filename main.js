134118a4a36fb7debb71cde560ad6806b47f4b30
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-11-08 15:45:39 +0100

    Add convenience function for fixed string serving

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 
 const http = require('http');
 
-const { stringResponder } = require('./lib/filecache.js');
 const httplog = require('./lib/httplog.js');
 const { Router } = require('./lib/router.js');
 
 const r = new Router();
 
 // Landing page
-r.get('/', stringResponder(200, 'Hello World!'));
+r.getString('/', 200, 'Hello World!');
 
 // Statics
 r.alias('/favicon.ico', '/static/icon.ico');
 r.getStatic(/\/static\/(.*)/, '$1', 'static');
 
 // Catch-all routes
-r.route('GET', null, stringResponder(404, '404 Not Found'));
-r.route(null, null, stringResponder(405, '405 Method Not Allowed'));
+r.routeString('GET', null, 404, '404 Not Found');
+r.routeString(null, null, 405, '405 Method Not Allowed');
 
 http.createServer(httplog.wrap(r.makeCallback())).listen(8080);
